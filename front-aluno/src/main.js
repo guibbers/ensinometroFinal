f298c2ad5336c77506666c7bb9ddad6c6ecb4440
Guilherme Torres <guiatorres8@gmail.com>
2021-09-07 04:02:21 -0300

    finish main

--- front-aluno/src/main.js
+++ front-aluno/src/main.js
@@ -1,14 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
+import Vue from 'vue';
+import App from './App.vue';
+import vuetify from './plugins/vuetify';
 import io from 'socket.io-client';
 import VueSocketIO from 'vue-socket.io';
 import 'animate.css';
+import router from './router/index.js'
 
+Vue.config.productionTip = false;
 
-Vue.config.productionTip = false
-
-const connection = io.connect('http://localhost:3000', {
+const connection = io.connect('https://ensinometro-back.herokuapp.com', {
   transports: ['websocket'],
   upgrade: false
 });
@@ -19,6 +19,7 @@ Vue.use(new VueSocketIO({
 }));
 
 new Vue({
+  router,
   vuetify,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
